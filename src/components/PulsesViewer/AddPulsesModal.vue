fb8111a58c7baee7c599d88e10d3d2d97e6e8de3
avfrancev <avfrancev@ya.ru>
2024-08-28 11:47:47 +0300

    Added examples for using data formats

--- src/components/PulsesViewer/AddPulsesModal.vue
+++ src/components/PulsesViewer/AddPulsesModal.vue
@@ -2,15 +2,20 @@
 Modal
   template(#trigger)
     DialogTrigger(class="join-item btn btn-square bg-opacity-0 border-0")
-      //- i-ph:file-plus-fill
-      //- i-pajamas:doc-new
       i-mingcute:file-new-line(class="text-lg")
   template(#content)
     DialogTitle(class="mb-4 text-lg font-bolds")
       | Add new pulses
     DialogDescription(class="text-mauve11 mt-[10px] mb-5 text-[15px] leading-normal")
-      pre(class="text-xs text-base-content/50") Example: 434,394,380,422,379,422,377,421,378,420,377,421
-      pre(class="text-xs text-base-content/50") Example: 434,394,380,422,379,422,377,421,378,420,377,421
+      div(class="text-xs text-base-content/50") 
+        pre You can use:
+        pre Plain: 434,394,380,422,379,422,377,421,378,420,377,421
+        pre or
+        pre
+          | 244,355
+          | 244,355
+        pre or
+        pre RFRaw: AAB03505141306060E02C601902378A481A3B2B2A3B2A3A3B2B2B2A3A3B2B2A3B2A3B2A3A3B2A3A3B2A3B2A3A3B2B2... 
     textarea(
       v-model="tmpPulsesString"
       placeholder="434,394,380,422,379,422,377,421,378,420,377,421"     class="textarea textarea-bordered my-4 flex-1 w-full")
