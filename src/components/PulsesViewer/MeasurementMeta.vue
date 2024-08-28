1676fa72114aa351c9138a07678ea378e4c52f76
avfrancev <avfrancev@ya.ru>
2024-08-28 11:48:09 +0300

    Update MeasurementMeta

--- src/components/PulsesViewer/MeasurementMeta.vue
+++ src/components/PulsesViewer/MeasurementMeta.vue
@@ -35,7 +35,7 @@ div(
         button(class="join-item btn-xs btn flex-1" @click="m.descOpened = !m.descOpened")
           i-mingcute:right-fill(v-if="!m.descOpened")
           i-mingcute:left-fill(v-if="m.descOpened")
-    //- div(class="flex flex-col" v-show="m.descOpened")
+    div(class="flex flex-col" v-show="m.descOpened")
       div(class="ml-4 mb-2 join flex justify-stretch")
         button(
           class="join-item btn btn-xs"
@@ -46,19 +46,21 @@ div(
       div(class="flex-1 ml-4 more flex flex-col overflow-hidden max-w-lg overflow-x-auto text-xs")
         div(class="flex join")
         //- pre {{ m.decoder.analyzer.guessed }}
-        p(class="font-mono text-nowrap" v-if="m.decoder.analyzer?.guessed")
-          |#[b Modulation]:
-          | [ {{m.decoder.analyzer.guessed.modulation}} ]
-          | {{m.decoder.analyzer.guessed.short && "short: " + m.decoder.analyzer.guessed.short.toFixed(1)}}
-          | {{m.decoder.analyzer.guessed.long && "long: " + m.decoder.analyzer.guessed.long.toFixed(1)}}
-          | {{m.decoder.analyzer.guessed.sync && "sync: " + m.decoder.analyzer.guessed.sync.toFixed(1)}}
-          | {{m.decoder.analyzer.guessed.gap && "gap: " + m.decoder.analyzer.guessed.gap.toFixed(1)}}
-          | {{m.decoder.analyzer.guessed.reset && "reset: " + m.decoder.analyzer.guessed.reset.toFixed(1)}}
-        pre #[b Guessing modulation]: {{m.decoder.guess.name}}
+        p(class="font-mono text-nowrap" v-if="m.decoder.guess")
+        p
+          |#[b Timings]:
+          //- | [ {{m.decoder.guess?.modulation}} ]
+          | {{m.decoder.guess?.short && "short: " + m.decoder.guess.short.toFixed(1)}}
+          | {{m.decoder.guess?.long && "long: " + m.decoder.guess.long.toFixed(1)}}
+          | {{m.decoder.guess?.sync && "sync: " + m.decoder.guess.sync.toFixed(1)}}
+          | {{m.decoder.guess?.gap && "gap: " + m.decoder.guess.gap.toFixed(1)}}
+          | {{m.decoder.guess?.reset && "reset: " + m.decoder.guess.reset.toFixed(1)}}
+        pre #[b Guessing modulation]: {{m.decoder.guess?.name}}
         pre #[b DC bias (Pulse/Gap skew)]: {{(m.decoder.analyzer?.pulse_gap_skew * 100 || 0).toFixed(1)}}%
         pre #[b RfRaw (rx)]: {{m.decoder.analyzer?.rfrawB1}}
         pre #[b RfRaw (tx)]: {{m.decoder.analyzer?.rfrawB0}}
-        pre(class="text-balances") #[b Bits]: {{m.decoder.sliceGuess?.bits?.toHexString && m.decoder.sliceGuess?.bits?.toHexString()}}
+        pre(class="text-balances" ) #[b Bits]: {{ m.decoder.sliceGuess?.hex }}
+
     //- .ml-4.more.transition(v-show="m.descOpened")
       .flex.items-baseline
         pre CKMean clusters:
